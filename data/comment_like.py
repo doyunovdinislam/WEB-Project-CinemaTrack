b796206c52a676e79796fb6084f67e964439bbe1
ne7zzy8228-sketch <ne7zzy8228@gmail.com>
2026-05-12 14:23:56 +0300

    Delete data directory

--- data/comment_like.py
+++
@@ -1,9 +0,0 @@
-from .db import db
-
-class CommentLike(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    user_id = db.Column(db.Integer)
-    comment_id = db.Column(db.Integer)
-
-    value = db.Column(db.Integer)
