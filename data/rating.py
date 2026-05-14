3f4b927d12d8c27193ec5c4fb3b951c14649e1f3
ne7zzy8228-sketch <ne7zzy8228@gmail.com>
2026-05-14 15:28:42 +0300

    Delete data directory

--- data/rating.py
+++
@@ -1,9 +0,0 @@
-from .db import db
-
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
-
-    value = db.Column(db.Integer)
--- data/user_rating.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-
-class UserRating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    rater_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    value = db.Column(db.Integer)
